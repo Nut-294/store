f49e739c6de22f95271b01c9d47778fb58887c5d
Nut-294 <134218655+Nut-294@users.noreply.github.com>
2026-03-17 09:18:44 +0700

    Product Model

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,24 +1,4 @@
-import { prisma } from "@/lib/prisma";
-
-async function AboutPage() {
-  const profile = await prisma.testProfile.create({
-    data: {
-      name: "random name",
-    },
-  });
-
-  const users = await prisma.testProfile.findMany();
-
-  return (
-    <div>
-      {users.map((user) => {
-        return (
-          <h2 key={user.id} className="text-2xl font-bold">
-            {user.name}
-          </h2>
-        );
-      })}
-    </div>
-  );
+function AboutPage() {
+  return <div>About Page</div>;
 }
 export default AboutPage;
