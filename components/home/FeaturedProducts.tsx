26bb263940aea646371bdc0b3c1fdb7a19d1b0aa
Nut-294 <134218655+Nut-294@users.noreply.github.com>
2026-03-18 13:23:35 +0700

    Featured Products

--- components/home/FeaturedProducts.tsx
+++ components/home/FeaturedProducts.tsx
@@ -1,4 +1,17 @@
-function FeaturedProducts() {
-  return <div>FeaturedProducts</div>;
+import { fetchFeaturedProducts } from "@/utils/actions";
+import EmptyList from "../global/EmptyList";
+import SectionTitle from "../global/SectionTitle";
+import ProductGrid from "../products/ProductGrid";
+
+async function FeaturedProducts() {
+  const products = await fetchFeaturedProducts();
+  if (products.length === 0) return <EmptyList />;
+
+  return (
+    <section className="pt-24">
+      <SectionTitle text="featured products" />
+      <ProductGrid products={products} />
+    </section>
+  );
 }
 export default FeaturedProducts;
