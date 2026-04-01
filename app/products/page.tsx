f6dfa08eedab53d0ad93b93b9f7ffaa74b3e48dc
Nut-294 <134218655+Nut-294@users.noreply.github.com>
2026-04-01 11:16:51 +0700

    ProductsContainer Component

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,6 +1,14 @@
-function ProductsPage() {
-  return (
-    <div>ProductsPage</div>
-  )
+import ProductsContainer from "@/components/products/ProductsContainer";
+
+async function ProductsPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ layout?: string; search?: string }>;
+}) {
+  const searchParamsData = await searchParams;
+  console.log(searchParamsData);
+  const layout = searchParamsData.layout || "grid";
+  const search = searchParamsData.search || " ";
+  return <ProductsContainer layout={layout} search={search} />;
 }
-export default ProductsPage
+export default ProductsPage;
