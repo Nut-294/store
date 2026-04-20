4839fb3e7b99680bb3ba19e04446c79b26ba34d4
Nut-294 <134218655+Nut-294@users.noreply.github.com>
2026-04-20 10:05:42 +0700

    LinksDropdown Component

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,13 +1,17 @@
+"use client";
 import { LuUser } from "react-icons/lu";
-import { currentUser } from "@clerk/nextjs/server";
-async function UserIcon() {
-  const user = await currentUser();
+import { useUser } from "@clerk/nextjs";
+function UserIcon() {
+  const { user } = useUser();
+
   const profileImage = user?.imageUrl;
-  if (profileImage)
+  if (profileImage) {
     return (
       <img src={profileImage} className="w-6 h-6 rounded-full object-cover" />
     );
+  }
 
   return <LuUser className="w-6 h-6 bg-primary rounded-full text-white" />;
 }
+
 export default UserIcon;
