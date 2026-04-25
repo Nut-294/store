1cf2aea2362c62098d11e6ec0a2d55eea033e5f0
Nut-294 <134218655+Nut-294@users.noreply.github.com>
2026-04-25 11:06:15 +0700

    CreateProductAction(First) & Helper Functions

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,6 +1,17 @@
 "use server";
 import { prisma } from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
+import { error } from "console";
 import { redirect } from "next/navigation";
+
+//ดึง User
+const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) redirect("/");
+  return user;
+};
+
+const renderError = (error: unknown): { message: string } => {};
 
 export const fetchFeaturedProducts = async () => {
   const products = await prisma.product.findMany({
@@ -41,5 +52,30 @@ export const createProductAction = async (
   prevState: any,
   formData: FormData
 ): Promise<{ message: string }> => {
-  return { message: "product created" };
+  const user = await getAuthUser();
+  try {
+    const name = formData.get("name") as string;
+    const company = formData.get("company") as string;
+    const price = Number(formData.get("price") as string);
+    //temp
+    const image = formData.get("image") as File;
+    const description = formData.get("description") as string;
+    const featured = Boolean(formData.get("featured") as string);
+
+    await prisma.product.create({
+      data: {
+        name,
+        company,
+        price,
+        image: "/images/hero1.jpg",
+        description,
+        featured,
+        clerkId: user.id,
+      },
+    });
+
+    return { message: "product created" };
+  } catch (error) {
+    return renderError(error);
+  }
 };
