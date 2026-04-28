2f5b87cea5bca2663fd164dc23727bcc662687c4
Nut-294 <134218655+Nut-294@users.noreply.github.com>
2026-04-28 21:47:28 +0700

    Fetch Admin Products

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,6 +1,58 @@
-function ProductsPage() {
+import EmptyList from "@/components/global/EmptyList";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { fetchAdminProduct } from "@/utils/actions";
+import { formatCurrency } from "@/utils/format";
+import Link from "next/link";
+
+async function AdminProductsPage() {
+  const items = await fetchAdminProduct();
+  if (items.length === 0) return <EmptyList />;
   return (
-    <div>Admin ProductsPage</div>
-  )
+    <section>
+      <Table>
+        <TableCaption className="capitalize">
+          total products : {items.length}
+        </TableCaption>
+        {/* หัวตาราง */}
+        <TableHeader>
+          <TableRow>
+            <TableHead>Product Name</TableHead>
+            <TableHead>Company</TableHead>
+            <TableHead>Price</TableHead>
+            <TableHead>Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        {/* ข้อมูลในตาราง */}
+        <TableBody>
+          {items.map((item, index) => {
+            const { id: productId, name, company, price } = item;
+            return (
+              <TableRow key={index}>
+                <TableCell>
+                  <Link
+                    href={`/products/${productId}`}
+                    className="underline text-muted-foreground tracking-wide capitalize"
+                  >
+                    {name}
+                  </Link>
+                </TableCell>
+                <TableCell>{company}</TableCell>
+                <TableCell>{formatCurrency(price)}</TableCell>
+                <TableCell className="flex items-center gap-x-2"></TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </section>
+  );
 }
-export default ProductsPage
+export default AdminProductsPage;
--- utils/actions.ts
+++ utils/actions.ts
@@ -12,6 +12,13 @@ const getAuthUser = async () => {
   return user;
 };
 
+//เช็ค Admin
+const getAdminUser = async () => {
+  const user = await getAuthUser();
+  if (user.id !== process.env.ADMIN_USER_ID) redirect("/");
+  return user;
+};
+
 const renderError = (error: unknown): { message: string } => {
   console.log(error);
   return {
@@ -77,4 +84,14 @@ export const createProductAction = async (
     return renderError(error);
   }
   redirect("/admin/products");
+};
+
+export const fetchAdminProduct = async () => {
+  await getAdminUser();
+  const products = await prisma.product.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  return products;
 };
