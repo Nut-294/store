ce4329db576abcc1a975236619e8e7d753b6d668
Nut-294 <134218655+Nut-294@users.noreply.github.com>
2026-05-01 16:11:10 +0700

    Edit Product Page

--- app/admin/products/[id]/edit/page.tsx
+++ app/admin/products/[id]/edit/page.tsx
@@ -1,6 +1,53 @@
-function EditProductPage() {
+import { SubmitButton } from "@/components/form/Button";
+import CheckboxInput from "@/components/form/CheckboxInput";
+import FormContainer from "@/components/form/FormContainer";
+import FormInput from "@/components/form/FormInput";
+import PriceInput from "@/components/form/PriceInput";
+import TextAreaInput from "@/components/form/TextAreaInput";
+import { fetchAdminProductDetails, updateProductAction } from "@/utils/actions";
+
+async function EditProductPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  console.log("id", id);
+  const product = await fetchAdminProductDetails(id);
+  const { name, company, description, featured, price } = product;
   return (
-    <div>EditProductPage</div>
-  )
+    <section>
+      <h1 className="text-2xl font-semibold mb-8 capitalize">update product</h1>
+      <div className="border p-8 rounded">
+        {/* Image input container*/}
+        <FormContainer action={updateProductAction}>
+          <div className="grid gap-4 md:grid-cols-2 my-4">
+            <input type="hidden" name="id" value={id} />
+            <FormInput
+              type="text"
+              name="name"
+              label="product name"
+              defaultValue={name}
+            />
+            <FormInput type="text" name="company" defaultValue={company} />
+            <PriceInput defaultValue={price} />
+          </div>
+          <TextAreaInput
+            name="description"
+            labelText="product description"
+            defaultValue={description}
+          />
+          <div className="mt-6">
+            <CheckboxInput
+              name="featured"
+              label="featured"
+              defaultChecked={featured}
+            />
+          </div>
+          <SubmitButton text="update product" className="mt-8" />
+        </FormContainer>
+      </div>
+    </section>
+  );
 }
-export default EditProductPage
+export default EditProductPage;
