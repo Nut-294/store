6f871b018b8463dc52bba6189e457146d19ba20b
Nut-294 <134218655+Nut-294@users.noreply.github.com>
2026-05-18 20:58:55 +0700

    Delete Cart Item Action

--- components/cart/ThirdColumn.tsx
+++ components/cart/ThirdColumn.tsx
@@ -1,6 +1,30 @@
-function ThirdColumn() {
+"use client";
+import { useState } from "react";
+import SelectProductAmount, {
+  Mode,
+} from "../single-product/SelectProductAmount";
+import FormContainer from "../form/FormContainer";
+import { removeCartItemAction } from "@/utils/actions";
+import { SubmitButton } from "../form/Button";
+
+function ThirdColumn({ quantity, id }: { quantity: number; id: string }) {
+  const [amount, setAmount] = useState(quantity);
+  const handleAmountChange = async (value: number) => {
+    setAmount(value);
+  };
   return (
-    <div>ThirdColumn</div>
-  )
+    <div className="md:ml-8">
+      <SelectProductAmount
+        amount={amount}
+        setAmount={handleAmountChange}
+        mode={Mode.CartItem}
+        isLoading={false}
+      />
+      <FormContainer action={removeCartItemAction}>
+        <input type="hidden" name="id" value={id} />
+        <SubmitButton size="sm" className="mt-4" text="remove" />
+      </FormContainer>
+    </div>
+  );
 }
-export default ThirdColumn
+export default ThirdColumn;
