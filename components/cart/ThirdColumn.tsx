2472cc4570271d297f076c3e4c7cc627e1711c18
Nut-294 <134218655+Nut-294@users.noreply.github.com>
2026-05-19 11:16:19 +0700

    Edit Cart Item Action

--- components/cart/ThirdColumn.tsx
+++ components/cart/ThirdColumn.tsx
@@ -4,13 +4,24 @@ import SelectProductAmount, {
   Mode,
 } from "../single-product/SelectProductAmount";
 import FormContainer from "../form/FormContainer";
-import { removeCartItemAction } from "@/utils/actions";
+import { removeCartItemAction, updateCartItemAction } from "@/utils/actions";
 import { SubmitButton } from "../form/Button";
+import { toast } from "sonner";
 
 function ThirdColumn({ quantity, id }: { quantity: number; id: string }) {
   const [amount, setAmount] = useState(quantity);
+  const [isLoading, setIsLoading] = useState(false);
+
   const handleAmountChange = async (value: number) => {
+    setIsLoading(true);
+    toast("Calculating...");
+    const result = await updateCartItemAction({
+      amount: value,
+      cartItemId: id,
+    });
     setAmount(value);
+    toast(result.message);
+    setIsLoading(false);
   };
   return (
     <div className="md:ml-8">
