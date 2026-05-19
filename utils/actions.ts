f607313a76a0dc59aa09187cd4741f77a9072807
Nut-294 <134218655+Nut-294@users.noreply.github.com>
2026-05-19 12:27:59 +0700

    --- Bug Fix ---

--- utils/actions.ts
+++ utils/actions.ts
@@ -452,6 +452,9 @@ export const updateCart = async (cart: Cart) => {
     include: {
       product: true,
     },
+    orderBy:{
+      createdAt:'asc'
+    }
   });
   let numItemsInCart = 0;
   let cartTotal = 0;
@@ -476,7 +479,7 @@ export const updateCart = async (cart: Cart) => {
     },
     include: includeProductClause,
   });
-  return currentCart;
+  return {cartItems,currentCart};
 };
 
 export const addToCartAction = async (prevState: any, formData: FormData) => {
