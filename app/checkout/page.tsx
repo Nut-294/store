c059c72e087ef4c0213526b3234a6d2dd159c16e
Nut-294 <134218655+Nut-294@users.noreply.github.com>
2026-05-23 19:42:01 +0700

    -- Fix Checkout Page 2--

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,6 +1,6 @@
+"use client";
 export const dynamic = "force-dynamic";
 
-"use client";
 import axios from "axios";
 import { useSearchParams } from "next/navigation";
 import { useCallback } from "react";
